f6640058893420d2f547a94682d5233813d4679a
CubeBlack <deltalafalambda@gmail.com>
2018-09-09 07:04:39 -0300

    detalhe na nuvem
    Pequena auteracoa para detalhar  nuvem de dialogos

--- js/dialogoNuvem.js
+++ js/dialogoNuvem.js
@@ -6,13 +6,17 @@ dialogo.rListar = function (msg) {
 	console.log(objDialogos[0].entrada);
 	retorno = "";
 	for (i = 0; i < objDialogos.length;i++) {
-		dados = {"entrada":objDialogos[i].entrada};
+		dados = {
+			"entrada":objDialogos[i].entrada,
+			"quantidade":objDialogos[i]["count(*)"],
+			"tamanho":"30"
+			};
 		retorno += page.replace(dialogo.tDialogo,dados);
 	}
 	dialogo.eleContent.innerHTML = retorno;
 	return;
 }
-dialogo.tDialogo = "<a href='forum.php?tipo=dialogo&titulo={entrada}'>{entrada}</a>";
+dialogo.tDialogo = "<a style='font-size:{tamanho}px;' href='forum.php?tipo=dialogo&titulo={entrada}'>{entrada}<SUP>{quantidade}</SUP></a>";
 dialogo.listar = function () {
 	term.com("dialogo.listar( ,json);",dialogo.rListar);
 }
